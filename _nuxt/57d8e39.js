a866ee700bd118256f67e2ac086c03f34804868a
Roy <royketelaar@gmail.com>
2021-03-10 11:12:23 +0100

    Delete _nuxt directory

--- _nuxt/57d8e39.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{142:function(n,t,e){"use strict";var c=e(31),component=Object(c.a)({},(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("Nuxt")],1)}),[],!1,null,null,null);t.a=component.exports},143:function(n,t,e){e(144),n.exports=e(145)}},[[143,3,1,4]]]);
